23ad41b6fa77c28552f7a73679eebd96dbfe89c6
Ronit Savaliya <ronit.savaliya@noovosoft.com>
2026-08-31 11:55:42 +0530

    Add form data in local storage

---
+++ css-portfolio/index.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (event){
+
+    event.preventDefault();
+
+   const name = document.getElementById("name").value;
+   const email = document.getElementById("email").value;
+   const message = document.getElementById("message").value;
+
+   const data = {
+       name: name,
+       email: email,
+       message: message
+   }
+
+   // localStorage.setItem("name", name);
+   // localStorage.setItem("email", email);
+   // localStorage.setItem("message", message);
+
+   localStorage.setItem("data", JSON.stringify(data));
+
+   form.reset();
+});
